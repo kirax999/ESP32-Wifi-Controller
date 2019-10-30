c0687ba5bab7f971b7db5fa4f7750c23048abe2a
Alexandre Richet <alexandre@richet.me>
2019-10-30 13:47:49 +0100

    previous test other wifimanger setup

--- WifiController/src/main.cpp
+++ WifiController/src/main.cpp
@@ -18,16 +18,21 @@
 #define LED       2
 #define INTERVAL  1000
 
+#define PORTUDP   1667
+#define BUFFERSIZE 1024
+
 char data;
 unsigned long int timer;
 bool isConnected = false;
-
 int statusSpeed = 0;
-
 WiFiManager wifiManager;
 
-void configModeCallback (WiFiManager *myWiFiManager)
-{
+WiFiUDP udp;
+char buffer[BUFFERSIZE];
+
+IPAddress targetIP;
+
+void configModeCallback (WiFiManager *myWiFiManager) {
     Serial.println("Entered config mode");
     Serial.println(WiFi.softAPIP());
     Serial.println(myWiFiManager->getConfigPortalSSID());
@@ -37,16 +42,18 @@ void setup()
 {
     Serial.begin(9600);
     pinMode(LED,OUTPUT);
-    wifiManager.setDebugOutput(false);
+    wifiManager.setDebugOutput(true);
     wifiManager.setAPCallback(configModeCallback);
     wifiManager.setTimeout(60);
-    if(!wifiManager.autoConnect("Controller")) {
+    if(!wifiManager.autoConnect("ESP32 Controller")) {
         Serial.println("failed to connect and hit timeout");
         statusSpeed = INTERVAL;
     } else {
         Serial.println("Successfully connected");
         isConnected = true;
         statusSpeed = INTERVAL / 4;
+        udp.begin(PORTUDP);
+        Serial.println("Ready !!!");
     }
     
     Serial.print("IP address: ");
@@ -55,13 +62,9 @@ void setup()
 
 void loop()
 {
-    if(millis()-timer>=INTERVAL) {
+    if(millis()-timer>=statusSpeed) {
         timer=millis();
         digitalWrite(LED,!digitalRead(LED));
-        if(digitalRead(LED)==LOW)
-            Serial.println("LED OFF");
-        else
-            Serial.println("LED ON");
     } 
 
     if(Serial.available()) {
@@ -77,6 +80,12 @@ void loop()
     }
 
     if (isConnected) {
-
+        memset(buffer, 0, BUFFERSIZE);
+        udp.parsePacket();
+        if (udp.read(buffer, BUFFERSIZE) > 0) {
+            if (strcmp(buffer, "123456") == 0) {
+                targetIP = udp.remoteIP();
+            }
+        }
     }
 }
