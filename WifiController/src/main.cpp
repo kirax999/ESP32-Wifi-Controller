b13116d9a8615ecefee8d5f3dbb118cd1eca84a3
Alexandre Richet <alexandre@richet.me>
2019-11-02 22:29:07 +0100

    Previous organisation data

--- WifiController/src/main.cpp
+++ WifiController/src/main.cpp
@@ -16,21 +16,19 @@
 #include <WiFiManager.h>
 #include <Wire.h>
 
-#include <MPU6050.h>
-#include <I2Cdev.h>
 #include <math.h>
 
 #include <ArduinoJson.h>
 
-#define LED       2
-#define INTERVAL  1000
+#define LED         2
+#define INTERVAL    1000
 
 #define SSF_GYRO    65.5
 #define FREQ        250
 
-#define PORTUDP   1667
-#define PORTUDPSEND   1668
-#define BUFFERSIZE 1024
+#define PORTUDP     1667
+#define PORTUDPSEND 1668
+#define BUFFERSIZE  1024
 
 struct Vector3 {
     double x;
@@ -52,7 +50,6 @@ char data;
 unsigned long int timer;
 bool isConnected = false;
 int statusSpeed = 0;
-int period = 0;
 
 WiFiManager wifiManager;
 IPAddress targetIP;
@@ -63,12 +60,14 @@ char buffer[BUFFERSIZE];
 Vector3 offset;
 Vector3 result;
 
+int minVal=265;
+int maxVal=402;
+
 float angle = 0;
 
 // Define function
 Vector3 getAngles();
 MPUValue getAngleBrute();
-MPUValue readSensor();
 void calibrateMpu6050();
 void setupMpu6050();
 
@@ -87,7 +86,6 @@ void setup()
 
     setupMpu6050();
     calibrateMpu6050();
-    period = (1000000/FREQ);
 }
 
 void loop()
@@ -164,11 +162,18 @@ MPUValue getAngleBrute() {
 }
 
 Vector3 getAngles() { // return Struc Gyroscope containe 3axis
-    MPUValue tmp = readSensor();
+    MPUValue tmp = getAngleBrute();
+    double AcX = (tmp.gyrX - offset.x);
+    double AcY = (tmp.gyrY - offset.y);
+    double AcZ = (tmp.gyrZ - offset.z);
 
-    result.x = (tmp.gyrX - offset.x) / (250 * SSF_GYRO);
-    result.y = (tmp.gyrY - offset.y) / (250 * SSF_GYRO);
-    result.z = (tmp.gyrZ - offset.z) / (250 * SSF_GYRO);
+    int x = map(AcX,minVal,maxVal,-90,90);
+    int y = map(AcY,minVal,maxVal,-90,90);
+    int z = map(AcZ,minVal,maxVal,-90,90);
+
+    result.x = RAD_TO_DEG * (atan2(-y, -z)+PI);
+    result.y = RAD_TO_DEG * (atan2(-x, -z)+PI);
+    result.z = RAD_TO_DEG * (atan2(-y, -x)+PI);
 
     Serial.print("x:");
     Serial.print(result.x);
@@ -176,34 +181,15 @@ Vector3 getAngles() { // return Struc Gyroscope containe 3axis
     Serial.print(result.y);
     Serial.print("\t z:");
     Serial.println(result.z);
+    Serial.println("------------------------------");
     return result;
 }
 
 void setupMpu6050() {
     Wire.begin();
-
-    // Configure power management
-    Wire.beginTransmission(0x68); // Start communication with MPU
-    Wire.write(0x6B);                    // Request the PWR_MGMT_1 register
-    Wire.write(0x00);                    // Apply the desired configuration to the register
-    Wire.endTransmission(true);
-
-    // Configure the gyro's sensitivity
-    Wire.beginTransmission(0x68); // Start communication with MPU
-    Wire.write(0x1B);                    // Request the GYRO_CONFIG register
-    Wire.write(0x08);                    // Apply the desired configuration to the register : ±500°/s
-    Wire.endTransmission(true);
-
-    // Configure the acceleromter's sensitivity
-    Wire.beginTransmission(0x68); // Start communication with MPU
-    Wire.write(0x1C);                    // Request the ACCEL_CONFIG register
-    Wire.write(0x10);                    // Apply the desired configuration to the register : ±8g
-    Wire.endTransmission(true);
-
-    // Configure low pass filter
-    Wire.beginTransmission(0x68); // Start communication with MPU
-    Wire.write(0x1A);                    // Request the CONFIG register
-    Wire.write(0x03);                    // Set Digital Low Pass Filter about ~43Hz
+    Wire.beginTransmission(0x68);
+    Wire.write(0x6B);
+    Wire.write(0);
     Wire.endTransmission(true);
 }
 
@@ -232,43 +218,47 @@ void calibrateMpu6050() {
     Serial.print("\n");
     Serial.println("--------------");
 }
-
-/*
-
-#include<Wire.h>
-
-const int MPU_addr=0x68;
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-
-int minVal=265;
-int maxVal=402;
-
-double x;
-double y;
-double z;
-
-void setup(){
-Wire.begin();
-Wire.beginTransmission(MPU_addr);
-Wire.write(0x6B);
-Wire.write(0);
-Wire.endTransmission(true);
-Serial.begin(9600);
-}
-void loop(){
-Wire.beginTransmission(MPU_addr);
-Wire.write(0x3B);
-Wire.endTransmission(false);
-Wire.requestFrom(MPU_addr,14,true);
-AcX=Wire.read()<<8|Wire.read();
-AcY=Wire.read()<<8|Wire.read();
-AcZ=Wire.read()<<8|Wire.read();
-int xAng = map(AcX,minVal,maxVal,-90,90);
-int yAng = map(AcY,minVal,maxVal,-90,90);
-int zAng = map(AcZ,minVal,maxVal,-90,90);
-
-x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
-y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
-z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
-}
 */
+
+#include <Wire.h>  // Arduino Wire library
+#include <I2Cdev.h>
+#include <MPU6050.h>
+#include <math.h>
+ 
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+ 
+int16_t ax, ay, az;  //mesures brutes
+int16_t gx, gy, gz;
+uint8_t Accel_range;
+uint8_t Gyro_range;
+float angle=0;
+
+#define BUFFER_LENGTH 1024
+ 
+void setup() {
+    Wire.begin();  //I2C bus
+    Serial.begin(9600);
+    while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB (LEONARDO)
+    }
+
+    // initialize device
+    Serial.println("Initialisation I2C...");
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Test de la conection du dispositif ...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection reussie" : "MPU6050 connection echec");
+    delay(1000);
+}
+ 
+void loop() {
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    //angle=0.98*(angle+float(gy)*0.01/131) + 0.02*atan2((double)ax,(double)az)*180/PI; // Y
+    //angle=0.98*(angle+float(gx)*0.01/131) + 0.02*atan2((double)ay,(double)az)*180/PI; // X
+    angle=0.98*(angle+float(gz)*0.01/131) + 0.02*atan2((double)ax,(double)ay)*180/PI; // Z
+    Serial.println(angle); 
+    delay(10);
+}
